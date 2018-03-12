7f5c6c4c98814c964706a5d2b323b7e29674960e
Jordan Simon <jns6971@gmail.com>
2018-03-11 20:18:23 -0700

    start creating a react version

---
+++ react-version/scripts/main.js
@@ -0,0 +1,37 @@
+require.config({
+	'baseUrl': '/scripts/lib',
+	'paths': {
+		'jquery' : 'jquery-1.11.0.min',
+		'bPopup': 'jquery.bpopup.min',
+		'timelineMax': 'TimelineMax.min',
+		'tweenMax': 'TweenMax.min',
+		'gsap': 'jquery.gsap.min',
+		'underscore': 'underscore-min',
+		'underscoreTemplate': 'underscoreTemplateConfig',
+		'oliverApp' : '../oliverApp'
+	},
+	'shim': {
+		'underscore' : {},
+		'underscoreTemplate': {},
+		'bPopup': {
+			deps: ['jquery']
+		},
+		'gsap': {
+			deps: ['jquery', 'timelineMax', 'tweenMax']
+		}
+	}
+});
+
+require(['underscore', 'jquery', 'oliverApp', 'bPopup', 'gsap', 'underscoreTemplate'], function(_, $, OliverApp) {
+	'use strict';
+
+	OliverApp.init({});
+    
+});
+
+function pop(dom_id){
+	$(dom_id).bPopup({
+		transition: 'slideDown',
+		speed: 'slow'
+	});
+}
